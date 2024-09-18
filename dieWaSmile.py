a2c0acc045b99c61f3d16bf9b6d4857db240b186
Huenyeon <alhenajulienne.gedalanga-21@cpu.edu.ph>
2024-09-18 14:37:05 +0800

    removed lyrics

--- dieWaSmile.py
+++ dieWaSmile.py
@@ -7,8 +7,6 @@ def lyrics():
     lines =[
         ("So Imma love you every night like its's the last niiight", 0.06),
         ("Like it's the last niiight", 0.06),
-        ("If the world is ending", 0.06),
-        ("I'd wanna be next to you", 0.08),
     ]
 
     delays = [0.6, 0.7]
