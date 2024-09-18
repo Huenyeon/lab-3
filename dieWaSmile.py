7b3a3d1b222ad53b960cd6dfcd71cf844491e997
Huenyeon <alhenajulienne.gedalanga-21@cpu.edu.ph>
2024-09-18 14:23:44 +0800

    file with bugs

---
+++ dieWaSmile.py
@@ -0,0 +1,23 @@
+import time
+from threading import Thread
+from time import sleep
+import sys
+
+def lyrics():
+    lines =[
+        ("So Imma love you every night like its's the last niiight", 0.06),
+        ("Like it's the last niiight", 0.06),
+    ]
+
+    delays = [0.6, 0.7]
+
+    for i, (line, char_delay) in enumerate(line):
+        for char in line:
+            print(char, end= '')
+            sys.stdout.flush()
+            sleep(chr_delay)
+
+        time.sleep(delays[i])
+        print('')
+
+lyrics()
